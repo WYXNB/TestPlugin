9801ad073bc1b845dfc76b65abc3a8e8599e698e
GitHub Action <action@github.com>
2022-07-01 09:32:44 +0000

    Template cleanup

--- src/main/kotlin/com/github/wyxnb/testplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/wyxnb/testplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.wyxnb.testplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.wyxnb.testplugin.MyBundle
 
 class MyApplicationService {
 
